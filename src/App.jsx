1ca58db5c12b35977f5ee77807151d89ff7c6af8
tom_brandi <brandner.thomas@me.com>
2026-01-11 11:38:20 +0100

    delete navbar example

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Navbar from './assets/components/Navbar'
 
 function App() {
     return (
 
         <>
-            <Navbar />
             <div className="container mt-3">
+                <p>Hallo Welt</p>
             </div>
         </>
     )
