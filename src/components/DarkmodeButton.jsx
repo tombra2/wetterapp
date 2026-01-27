94497f222608a2589c8d34dd9efd090f6e213f98
tom_brandi <brandner.thomas@me.com>
2026-01-27 17:09:10 +0100

    add darkmode button

---
+++ src/components/DarkmodeButton.jsx
@@ -0,0 +1,10 @@
+import './darkmodebutton.css';
+
+
+const DarkmodeButton = () => {
+    return (
+        <button className="btn btn-dark">Darkmode</button>
+    )
+}
+
+export default DarkmodeButton;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import './navbar.css'
+import DarkmodeButton from "./DarkmodeButton.jsx";
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-primary">
-            <div className="container-fluid">
+            <div className="container d-flex justify-content-between">
                 <span className="navbar-brand mb-0 h1">
                     <i className="bi bi-cloud-sun"></i> Wetterstation
+                </span>
+                <span>
+                <DarkmodeButton/>
                 </span>
             </div>
         </nav>
