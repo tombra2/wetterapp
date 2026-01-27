f842509f98194af793d744334768ca61a8f9d8cc
tom_brandi <brandner.thomas@me.com>
2026-01-27 15:18:14 +0100

    add gemini api incl handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
 import Navbar from "./components/Navbar.jsx"
 import Searchbar from "./components/Searchbar.jsx"
 import Weather from "./components/Weather.jsx"
+import Aiapi from "./components/Aiapi.jsx";
 
 function App() {
     const [city, setCity] = useState('')
+    const [weatherData, setWeatherData] = useState('')
 
     const handleSearch = (searchCity) => {
         setCity(searchCity)
     }
-
+    const handleWeather = (data) => {
+        setWeatherData(data)
+    }
     return (
         <>
-            <Navbar />
+            <Navbar/>
             <div className="container mt-4">
-                <Searchbar onSearch={handleSearch} />
+                <Searchbar onSearch={handleSearch}/>
                 <div className="row justify-content-center">
                     <div className="col-md-8">
-                        <Weather city={city} />
+                        <Weather city={city} onWeatherData={handleWeather}/>
+                        <Aiapi weatherData={weatherData} city={city}/>
                     </div>
                 </div>
             </div>
---
+++ src/components/Aiapi.jsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import './aiapi.css';
+import {GoogleGenAI} from "@google/genai";
+
+
+const API_KEY = import.meta.env.VITE_GEMINI_API_KEY
+const genAi = new GoogleGenAI({apiKey: API_KEY})
+
+
+const Aiapi = ({weatherData, city}) => {
+    const [response, setResponse] = useState('')
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const weatherDataString = JSON.stringify(weatherData);
+
+    const handleAiCall = async () => {
+        try {
+            setLoading(true)
+
+            const res = await genAi.models.generateContent({
+                model: "gemini-2.5-flash",
+                contents: `Was kann ich in ${city} machen? wenn das Wetter so ist ${weatherDataString}`,
+            })
+
+            setResponse(res.text);
+        } catch (error) {
+            setError(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    if (!city) return null;
+
+    return (
+        <div className="card">
+            <div className="card-header">
+                <button
+                    className="btn btn-primary "
+                    onClick={handleAiCall}
+                    disabled={loading}
+                >
+                    {loading ? 'Loading...' : `Was kann ich in ${city} machen ?`}
+                </button>
+            </div>
+            <div className="card-body d-flex justify-content-center">
+                {loading && (<div className="spinner-loading " role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>)}
+                {error && (<div className="alert alert-danger">{error.message}</div>)}
+                {response}
+            </div>
+        </div>
+    )
+}
+
+export default Aiapi;
